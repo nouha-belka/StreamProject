faeda2adbf477121e10b612c7d3db501e89f1e8c
nouha belkacemi <nouhabelka2001@gmail.com>
2026-02-18 14:54:11 -0500

    challenges from 35 to 38

--- src/main/java/org/blitmatthew/service/CarOps.java
+++ src/main/java/org/blitmatthew/service/CarOps.java
@@ -33,11 +33,33 @@ public class CarOps {
     public  List<Car> getManualCars(){
         return cars.stream().filter(x -> x.getTransmission().equals("Manual")).collect(Collectors.toList());
     }
-
+    
     // Find vehicles from specific manufacturers
     public List<Car> getCarsFromManufacturer(String... manufacturers){
         List<String> manufacturerList = Arrays.asList(manufacturers);
         return cars.stream().filter(x -> manufacturerList.contains(x.getMake()) ).collect(Collectors.toList());
     }
+    
+    //  List cars manufactured after 2020
+    public List<Car> getCarsAfter202(){
+        return cars.stream().filter(x -> x.getYear() > 2020).collect(Collectors.toList());
+    }
+    
+    //  Identify cars with mileage under 50,000
+    public List<Car> getCarsMileageUnder50k(){
+        return cars.stream().filter(x -> x.getMileage() <= 50000).collect(Collectors.toList());
+    }
+    
+    //  Find cars in "Excellent" condition
+    public  List<Car> getExcellentCars(){
+        return cars.stream().filter(x -> x.getCondition().equals("Manual")).collect(Collectors.toList());
+    }
+    
+    //  Filter cars by specific fuel types
+    public List<Car> getCarsByFuel(String... fuels){
+        List<String> fuelList = Arrays.asList(fuels);
+        return cars.stream().filter(x -> fuelList.contains(x.getFuelType()) ).collect(Collectors.toList());
+    }
+    
 
 }
